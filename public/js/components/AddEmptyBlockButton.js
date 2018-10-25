b27701d4d4cd9c4d7b58f1404f1aa6ab1b1c9491
vanzi <ivanl487@gmail.com>
2018-10-25 14:39:27 +0300

    base button added

---
+++ public/js/components/AddEmptyBlockButton.js
@@ -0,0 +1,11 @@
+class AddEmptyBlockButton extends BaseButton {
+    constructor (id, text, cssClass) {
+        super(id, text, cssClass);
+        this.handle = this.addEmptyBlock;
+    }
+
+    addEmptyBlock (initClass, event) {
+        $(event.target).before(initClass.getEmptyBlock());
+        initClass.initListeners();
+    }
+}
---
+++ public/js/components/BaseButton.js
@@ -0,0 +1,38 @@
+class BaseButton {
+    constructor (id, text, cssClass) {
+        this.template = '';
+        this.listeners = {
+            'click': {}
+        };
+        this.id = IdManipulation.getPreparedId('add-empty-block', id);
+        this.text = text ? text : '+ Добавить';
+        this.cssClass = cssClass;
+    }
+
+    makeTemplate () {
+        this.template = `<button id="${this.id}" class="btn ${this.cssClass}">${this.text}</button>`;
+    }
+
+    setListeners () {
+        Listeners.set(this, 'click', {
+            [this.id]: {
+                'function': this.handle,
+                'class': this,
+                'addInitClass': true,
+            }
+        });
+    }
+
+    getListeners () {
+        return this.listeners;
+    }
+    
+    getTemplate () {
+        return this.template;
+    }
+    
+    init () {
+        this.setListeners();
+        this.makeTemplate();
+    }
+}
