68e4a8f9204cdcf2461073689e48722fbbd6b8b8
vanzi <ivanl487@gmail.com>
2018-10-25 11:09:17 +0300

    listeners initclass to bind fix - 1

--- public/js/components/AddEmptyBlock.js
+++ public/js/components/AddEmptyBlock.js
@@ -1,28 +1,28 @@
 class AddEmptyBlock {
-    constructor (id, text, parent) {
+    constructor (id, text) {
         this.template = '';
         this.listeners = {
             'click': {}
         };
         this.id = IdManipulation.getPreparedId('add-empty-block', id);
         this.text = text ? text : '+ Добавить';
-        this.parent = parent;
     }
 
     makeTemplate () {
         this.template = `<button id="${this.id}" class="btn btn-primary">${this.text}</button>`;
     }
 
-    addEmptyBlock (event) {
-        $(event.target).before(this.parent.getEmptyBlock());
-        this.parent.initListeners();
+    addEmptyBlock (initClass, event) {console.log(event, this, initClass);
+        $(event.target).before(initClass.getEmptyBlock());
+        initClass.initListeners();
     }
 
     setListeners () {
         Listeners.set(this, 'click', {
             [this.id]: {
                 'function': this.addEmptyBlock,
-                'class': this
+                'class': this,
+                'addInitClass': true,
             }
         });
     }
