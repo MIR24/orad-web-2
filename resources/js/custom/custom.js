56bc92b056afe9da8e4f14c68fa41df00b189e3a
vanzi <ivanl487@gmail.com>
2018-11-22 15:27:36 +0300

    js loading logic updated

--- resources/js/custom/custom.js
+++ resources/js/custom/custom.js
@@ -6,28 +6,32 @@ import WeatherLive from "./Tabs/WeatherLive.js";
 import TimeShift from "./Tabs/TimeShift.js";
 import Countdown from "./Tabs/Countdown.js";
 import Promo from "./Tabs/Promo.js";
+import AdminControl from "./MultiTabs/AdminControl.js";
 
 var currentTab = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-    currentTab = new Tops();
-    currentTab.init();console.log(1, currentTab);
+    Array.from(document.querySelectorAll('a[id^=TabSwitch]')).forEach(tab => {
+        const formatters = {
+              'Tops': Tops,
+              'Newsbar': Newsbar,
+              'Expedited': Expedited,
+              'CurrencyValues': CurrencyValues,
+              'WeatherLive': WeatherLive,
+              'TimeShift': TimeShift,
+              'Countdown': Countdown,
+              'Promo': Promo,
+              'AdminControl': AdminControl,
+        };
 
-    var TabSwitches = document.querySelectorAll('a[id^=TabSwitch]');
-    Array.from(TabSwitches).forEach(tab => {
+        if (tab.attributes['data-tab-active'].value == 'true') {
+            currentTab = new formatters[tab.attributes['data-tab-name'].value]();
+            currentTab.init();
+        }
+
         tab.addEventListener('click', function(event) {
-            var formatters = {
-                  'Tops': Tops,
-                  'Newsbar': Newsbar,
-                  'Expedited': Expedited,
-                  'CurrencyValues': CurrencyValues,
-                  'WeatherLive': WeatherLive,
-                  'TimeShift': TimeShift,
-                  'Countdown': Countdown,
-                  'Promo': Promo,
-            }
             currentTab = new formatters[this.attributes['data-tab-name'].value]();
-            currentTab.init();console.log(2, currentTab);
+            currentTab.init();
         });
     })
 });
