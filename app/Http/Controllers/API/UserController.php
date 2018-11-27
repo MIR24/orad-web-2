175dd40867d0717abaf8fb8451524199027867f0
Konshin Alexander <kon2work@gmail.com>
2018-11-27 18:27:20 +0300

    Add mypermissions resource.

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Repositories\UserRepository;
 use App\Http\Controllers\API\BaseController;
 use App\Http\Resources\Common as CommonResource;
+use Illuminate\Http\Request;
 
 class UserController extends BaseController
 {
@@ -21,6 +22,17 @@ class UserController extends BaseController
         $this->middleware(['can:create_users'])->only(['store', 'storeMultiple']);
         $this->middleware(['can:update_users'])->only(['update', 'patchMultiple']);
         $this->middleware(['can:delete_users'])->only(['destroy']);
+    }
+
+    /**
+     * Display requestors resource permissions.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function showSelfPermissionsSelf(Request $request)
+    {
+        return $this->showPermissions($request->user()->id);
     }
 
     /**
