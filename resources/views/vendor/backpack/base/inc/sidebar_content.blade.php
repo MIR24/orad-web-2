12f38b80cadeb614b9fd1150fdddc48f53f66212
Konshin Alexander <kon2work@gmail.com>
2018-12-03 08:38:10 +0300

    Add CheckIfSuperAdmin middleware.

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,6 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ __('backpack::base.dashboard') }}</span></a></li>
+<!-- Users, Roles Permissions -->
+<li><a href="{{ backpack_url('user') }}"><i class="fa fa-user"></i> <span>{{ __('Users') }}</span></a></li>
+<li><a href="{{ backpack_url('role') }}"><i class="fa fa-group"></i> <span>{{ __('Roles') }}</span></a></li>
+<li><a href="{{ backpack_url('permission') }}"><i class="fa fa-key"></i> <span>{{ __('Permissions') }}</span></a></li>
