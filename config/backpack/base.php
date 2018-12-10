44b9abfe777dabec5fc4276d19df74a29d6f571d
Konshin Alexander <kon2work@gmail.com>
2018-12-10 12:03:23 +0300

    Change project name in backpack.

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,11 +12,11 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => config('app.name'),
 
     // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
-    'logo_mini' => '<b>B</b>p',
+    'logo_lg'   => '<b>ORAD</b>-WEB-2',
+    'logo_mini' => '<b>O</b>w-2',
 
     // Developer or company name. Shown in footer.
     'developer_name' => 'Cristian Tabacitu',
