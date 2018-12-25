39cffa0333d3aa55285341ef6d8f8781b4c01e9c
Konshin Alexander <kon2work@gmail.com>
2018-12-25 15:26:37 +0300

    Fix validation for roles and permissions.

--- config/backpack/permissionmanager.php
+++ config/backpack/permissionmanager.php
@@ -50,17 +50,40 @@ return [
     | Validation rules for requests
     |--------------------------------------------------------------------------
     |
+    | "%id%" token is used to exclude current record id from unique rule.
+    |
     */
 
     'validation' => [
+        'permisssion' => [
+            'store' => [
+                'name' => 'required|string|max:255|unique:permissions',
+            ],
+            'update' => [
+                'name' => 'required|string|max:255|unique:permissions,name,%id%',
+            ],
+        ],
+
         'role' => [
             'store' => [
                 'name' => 'required|string|max:255|unique:roles',
             ],
             'update' => [
+                'name' => 'required|string|max:255|unique:roles,name,%id%',
+            ],
+        ],
+
+        'user' => [
+            'store' => [
                 'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6|confirmed',
+            ],
+            'update' => [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users,email,%id%',
+                'password' => 'string|min:6|confirmed',
             ],
         ],
     ],
-
 ];
