de2b511b9640f71dab09d5fc1e1e7f791d2ee655
Konshin Alexander <kon2work@gmail.com>
2019-04-01 14:30:45 +0300

    Remove deleted records from select for TopsView.

---
+++ database/migrations/2019_04_01_141652_fix_tops_view_remove_deleted.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixTopsViewRemoveDeleted extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE OR REPLACE VIEW TopsView AS SELECT ((SELECT count(0) FROM Tops WHERE (t.id < Tops.id)) + 1) AS id, tc.text AS category, t.text AS text, (SELECT count(Tops.id)  FROM Tops  WHERE (Tops.category_id = t.category_id)) AS COUNT, (SELECT count(TopsCategories.id) FROM TopsCategories WHERE deleted_at IS NULL) AS themes_count FROM (Tops t  LEFT JOIN TopsCategories tc on((tc.id = t.category_id))) WHERE tc.deleted_at IS NULL AND t.deleted_at IS NULL ORDER BY id DESC');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS TopsView');
+    }
+}
